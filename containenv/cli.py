0a7fbd2dc2770c17304d25f80b957b0259bac65f
Za Wilgustus <wilcoxjg@gmail.com>
2016-11-21 14:40:44 -0700

    add basic template and renderer

--- containenv/cli.py
+++ containenv/cli.py
@@ -1,5 +1,29 @@
+import docker
+import jinja2
+import os
+import sys
+
+PKG_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+DEFAULT_DICT = {'IMAGE': 'ubuntu',
+                'TAG': 'latest',
+                'RUNCOMMANDS': 
+                    ['apt update && apt install -y python3\n       '+\
+                     '&& rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*']}
+def setup_target_as_python_environment(directory_name):
+    basictemplate = open(
+        os.path.join(PKG_ROOT, 'dockerfile_templates', 'basic')
+    ).read()
+    print(basictemplate)
+    print(jinja2.Template(basictemplate).render(**DEFAULT_DICT))
+
+
+
 def main():
-    pass
+    TARG_DIR = sys.argv[1]
+    print(PKG_ROOT)
+    print(os.listdir(PKG_ROOT))
+    setup_target_as_python_environment(sys.argv[1])
 
 if __name__ == '__main__':
     main()
--- setup.py
+++ setup.py
@@ -11,8 +11,9 @@ setup(
     license='Apache 2.0',
     url='https://github.com/zancas/containenv.git',
     packages=find_packages(),
-    install_requires=['docker-py >= 1.10.6, < 1.11'],
+    install_requires=['docker-py >= 1.10.6, < 1.11',
+                      'jinja2 >= 2.8, < 3'],
     entry_points={'console_scripts': [
-        'containenv=containenv.cli:main']}
-
+        'containenv=containenv.cli:main']},
+    package_data={'containenv':['dockerfile_templates/*']}
 )
