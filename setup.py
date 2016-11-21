971eb4a02136e2d3ec99fe8853bf55eab5bac2a8
Za Wilgustus <wilcoxjg@gmail.com>
2016-11-21 12:38:54 -0700

    Add cli.py, hack on setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,14 @@ import containenv
 print(containenv.__version__)
 setup(
     name='containenv',
-    version=containenv.__version__
+    version=containenv.__version__,
+    author='Za Wilgustus',
+    author_email='wilcoxjg@gmail.com',
+    license='Apache 2.0',
+    url='https://github.com/zancas/containenv.git',
+    packages=find_packages(),
+    install_requires=['docker-py >= 1.10.6, < 1.11'],
+    entry_points={'console_scripts': [
+        'containenv=containenv.cli:main']}
 
 )
