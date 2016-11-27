1417dd5607a0244fcc7c2708f1337acf93653e6b
Za Wilgustus <wilcoxjg@gmail.com>
2016-11-27 10:45:29 -0700

    begin adding argparse interface

--- protoenv/cli.py
+++ protoenv/cli.py
@@ -1,14 +1,17 @@
+import argparse
 import docker
 import jinja2
 import os
 import sys
 
 
-HOME = os.environ['HOME']
-CONTAINER_ENVS = os.path.abspath(
-    os.path.join(HOME, '.protoenv', 'environments')
+class ProtoEnvDirDoesNotExist(Exception):
+    pass
+
+CURRENT = os.path.abspath(os.curdir)
+PROTOENV = os.path.abspath(
+    os.path.join(CURRENT, '.protoenv')
 )
-print(HOME)
 PKG_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 DEFAULT_DICT = {'IMAGE': 'ubuntu',
@@ -17,31 +20,27 @@ DEFAULT_DICT = {'IMAGE': 'ubuntu',
                     ['apt update && apt install -y python3\n       '+\
                      '&& rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*']}
 
-def _init_config_dir(environment):
-    print(CONTAINER_ENVS)
-    print(environment)
-    env_path = CONTAINER_ENVS + environment
-    print(env_path)
-    if not os.path.isdir(env_path):
-        os.makedirs(env_path)
-    return env_path
+def _init_config_dir():
+    if not os.path.isdir(PROTOENV):
+        raise ProtoEnvDirDoesNotExist
 
-def set_python_config(environment, config_variables):
+def set_python_config(config_variables):
     basictemplate = open(
         os.path.join(PKG_ROOT, 'dockerfile_templates', 'basic')
     ).read()
     print(basictemplate)
-    env_path = _init_config_dir(environment)
+    _init_config_dir()
     rendered = jinja2.Template(basictemplate).render(**config_variables)
-    outdfile = open(os.path.join(env_path, 'Dockerfile'), 'w').write(rendered)
+    outdfile = open(os.path.join(PROTOENV, 'Dockerfile'), 'w').write(rendered)
 
 #def launch_a_python_environment(config_directory_context, 
 
 def main():
-    TARG_DIR = os.path.abspath(os.curdir)
-    print(PKG_ROOT)
-    print(os.listdir(PKG_ROOT))
-    set_python_config(TARG_DIR, DEFAULT_DICT)
+    usage='Initialize or run a virtualenv-like prototyping environment.'
+    parser = argparse.ArgumentParser(usage=usage)
+    print(CURRENT)
+    print(PROTOENV)
+    set_python_config(DEFAULT_DICT)
 
 if __name__ == '__main__':
     main()
