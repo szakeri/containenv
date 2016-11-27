5f4f47c4d9401a6cc8fe66d3360f764963284c64
Za Wilgustus <wilcoxjg@gmail.com>
2016-11-27 11:27:14 -0700

    protect the user from wrong protoenv

--- protoenv/cli.py
+++ protoenv/cli.py
@@ -5,7 +5,10 @@ import os
 import sys
 
 
-class ProtoEnvDirDoesNotExist(Exception):
+class ProtoEnvDockerfileDoesNotExist(Exception):
+    pass
+
+class ProtoEnvDockerfileAlreadyExists(Exception):
     pass
 
 CURRENT = os.path.abspath(os.curdir)
@@ -14,33 +17,39 @@ PROTOENV = os.path.abspath(
 )
 PKG_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-DEFAULT_DICT = {'IMAGE': 'ubuntu',
+PYTHON3 = {'IMAGE': 'ubuntu',
                 'TAG': 'latest',
                 'RUNCOMMANDS': 
                     ['apt update && apt install -y python3\n       '+\
                      '&& rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*']}
 
-def _init_config_dir():
-    if not os.path.isdir(PROTOENV):
-        raise ProtoEnvDirDoesNotExist
-
-def set_python_config(config_variables):
+def init_protoenv(config_variables):
     basictemplate = open(
         os.path.join(PKG_ROOT, 'dockerfile_templates', 'basic')
     ).read()
-    print(basictemplate)
-    _init_config_dir()
     rendered = jinja2.Template(basictemplate).render(**config_variables)
+    os.makedirs(PROTOENV)
     outdfile = open(os.path.join(PROTOENV, 'Dockerfile'), 'w').write(rendered)
 
-#def launch_a_python_environment(config_directory_context, 
+def run_protoenv():
+    pass
 
 def main():
     usage='Initialize or run a virtualenv-like prototyping environment.'
     parser = argparse.ArgumentParser(usage=usage)
-    print(CURRENT)
-    print(PROTOENV)
-    set_python_config(DEFAULT_DICT)
+    parser.add_argument( '--init',
+                         help='Initialize a new prototyping environement',
+                         action='store_true')
+    args = parser.parse_args()
+    if args.init:
+        if os.path.isfile(os.path.join(PROTOENV, 'Dockerfile')):
+            raise ProtoEnvDockerfileAlreadyExists
+        init_protoenv(PYTHON3)
+    else:
+        if not os.path.isfile(os.path.join(PROTOENV, 'Dockerfile')):
+            raise ProtoEnvDockerfileDoesNotExist
+        run_protoenv()
+        
 
 if __name__ == '__main__':
     main()
