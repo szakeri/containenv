86ef2693abb3f182c7df5f7d7a5a3bfc66929350
Za Wilgustus <wilcoxjg@gmail.com>
2016-12-23 19:34:40 +0000

    wip

---
+++ containenv/config/maps/__init__.py
@@ -0,0 +1,8 @@
+EXTENSION_MAP = { ".py": "python",
+                  ".c": "c",
+                  ".go": "go",
+                  ".js": "javascript",
+                  ".sh": "shell"}
+
+FILENAME_MAP = {"Makefile": "make",
+                ".git": "git"}
